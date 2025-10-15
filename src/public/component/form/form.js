f937eb877f8be7cdbbb645d9131efb730c6d03c2
kmi0817 <kmi0817@naver.com>
2025-10-15 18:56:38 +0900

    [feat] 공통 모듈인 form의 css, js 파일 작성

---
+++ src/public/component/form/form.js
@@ -0,0 +1,15 @@
+/**
+ * form 노드를 매개변수로 받아 form 내 필수 입력 값이 모두 입력됐는지 확인합니다
+ */
+export const validateRequiredInput = (form) => {
+    let isAllinput = true;
+
+    form.querySelectorAll('.form-input').forEach((e) => {
+        if (e.classList.contains('required') && !e.value) {
+            e.nextElementSibling.textContent = '* 값을 입력해 주세요';
+            isAllinput = false;
+        }
+    });
+
+    return isAllinput;
+};
