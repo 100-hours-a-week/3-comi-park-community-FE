5ff5a121cd26a4699b9c522737fb1f50d6d88846
kmi0817 <kmi0817@naver.com>
2025-10-15 17:45:26 +0900

    [feat] express.static 미들웨어 함수로 정적 디렉토리 설정

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
 const port = 3000;
+
+app.use(express.static(path.join(import.meta.dirname, 'public')));
 
 app.listen(port, () => {
     console.log(`Express runs on port ${port}`);
