54d749d10917510bfc42fea24845074e549273c9
kmi0817 <kmi0817@naver.com>
2025-10-18 23:35:11 +0900

    [refactor] index의 게시글 컨테이너에서 generateWriterInfoHtml() 활용하도록 수정

--- src/public/component/common/member/member.js
+++ src/public/component/common/member/member.js
@@ -1,7 +1,7 @@
 export const generateWriterInfoHtml = (member) => {
     // TODO: 회원 이미지 가져오기
     return `
-        <div class="writer-info" style="display:flex; align-items:center; gap 0.5em;">
+        <div class="writer-info" style="display:flex; align-items:center; gap: 0.5em;">
             <div class="writer-member-image">👤</div>
             <div class="writer-member-nickname">${member.nickname}</div>
         </div>`;
