239ca5a77e1a82276ad3c26b21682dec13a4964d
kmi0817 <kmi0817@naver.com>
2025-10-18 21:58:14 +0900

    [feat] postId 게시글 GET 요청 함수 작성

--- src/public/api/posts.js
+++ src/public/api/posts.js
@@ -35,6 +35,21 @@ export const requestWritePost = async (requestBody = {}) => {
     }
 };
 
+export const requestReadPost = async (postId) => {
+    try {
+        const res = await fetch(`${API_SERVER_URI}/posts/${postId}`, {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' },
+            credentials: 'include',
+        });
+        const json = await res.json();
+        return json;
+    } catch (error) {
+        console.error(error);
+        return { success: false, data: '문제가 발생했습니다' };
+    }
+};
+
 /**
  * lastPostId와 limit 필드로만 쿼리 스트링을 구성합니다
  * 단, lastPostId 혹은 limit이 숫자나 문자열 숫자일 때만 유효한 값으로 인정합니다
