20fb4ab484438c966f6aa1c25b2ec9b820c9de81
kmi0817 <kmi0817@naver.com>
2025-10-21 22:42:50 +0900

    [feat] 이메일, 닉네임 중복 체크 요청 디바운스로 구현 및 비밀번호, 비밀번호 확인도 디바운스로 검증하도록 리팩토링

--- src/public/register/register.js
+++ src/public/register/register.js
@@ -1,14 +1,9 @@
 import { validateRequiredInput, fieldValidationRules } from '../component/common/form/form.js';
-import { requestRegister } from '../api/members.js';
+import { requestEmailDuplicationCheck, requestNicknameDuplicationCheck, requestRegister } from '../api/members.js';
 
-const formInputKeyUpHandler = (name, target) => {
-    const value = target.value;
-    const helper = target.nextElementSibling;
-
-    const result = fieldValidationRules[name](value);
-
-    target.dataset.validated = result.isValidated;
-    helper.textContent = result.message;
+const requestMap = {
+    email: requestEmailDuplicationCheck,
+    nickname: requestNicknameDuplicationCheck,
 };
 
 const formSubmitBtnClickHandler = async () => {
@@ -32,30 +27,64 @@ const formSubmitBtnClickHandler = async () => {
     const res = await requestRegister(requestBody);
 
     if (!res.success) {
-        console.log(res);
-        document.querySelector('#form-nickname-input').nextElementSibling.textContent = res.data;
+        inputElements[0].nextElementSibling.textContent = res.data;
         return;
     }
 
     location.href = '/login';
 };
 
+function debouncedRequest(fn, delay = 200) {
+    let timer;
+    return function () {
+        const args = arguments;
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            fn.apply(this, args);
+        }, delay);
+    };
+}
+
+const formInputKeyUpHandler = (name, target) => {
+    const value = target.value;
+    const helper = target.nextElementSibling;
+
+    const result = fieldValidationRules[name](value);
+
+    target.dataset.validated = result.isValidated;
+    helper.textContent = result.message;
+
+    return result.isValidated;
+};
+
+const inputFormInputHandlerDebounced = (inputElement) => {
+    inputElement.addEventListener(
+        'input',
+        debouncedRequest(async function ({ target }) {
+            const isUnique = target.dataset.unique === 'true';
+            const fieldName = target.dataset.fieldname;
+            const fieldValue = target.value;
+
+            if (!formInputKeyUpHandler(fieldName, target)) {
+                // 유효한 값이 아니라면 중복 체크 요청을 보내지 않음
+                return;
+            }
+
+            if (!isUnique) {
+                // 중복 체크 요청 필요 없음
+                return;
+            }
+
+            const res = await requestMap[fieldName](fieldValue);
+
+            if (!res.success) {
+                target.nextElementSibling.textContent = res.data;
+            }
+        }, 400)
+    );
+};
+
 document.addEventListener('DOMContentLoaded', () => {
-    document.querySelector('#form-email-input').addEventListener('keyup', ({ target }) => {
-        formInputKeyUpHandler('email', target);
-    });
-
-    document.querySelector('#form-password-input').addEventListener('keyup', ({ target }) => {
-        formInputKeyUpHandler('password', target);
-    });
-
-    document.querySelector('#form-confirmed-password-input').addEventListener('keyup', ({ target }) => {
-        formInputKeyUpHandler('confirmedPassword', target);
-    });
-
-    document.querySelector('#form-nickname-input').addEventListener('keyup', ({ target }) => {
-        formInputKeyUpHandler('nickname', target);
-    });
-
+    document.querySelectorAll('.form-input').forEach((e) => inputFormInputHandlerDebounced(e));
     document.querySelector('.form-submit-btn').addEventListener('click', formSubmitBtnClickHandler);
 });
