a531eb4402c8d292052630bea4e601e102c86d86
kmi0817 <kmi0817@naver.com>
2025-10-22 14:07:22 +0900

    [feat] 회원 정보 조회 및 수정 요청 함수 작성

--- src/public/api/members.js
+++ src/public/api/members.js
@@ -11,3 +11,16 @@ export const requestNicknameDuplicationCheck = (nickname) => {
 export const requestRegister = (requestBody = {}) => {
     return request({ method: METHOD.POST, url: '/members', body: requestBody });
 };
+
+export const requestMemberInfo = (memberId) => {
+    return request({ url: `/members/${memberId}` });
+};
+
+export const requestMemberInfoUpdate = (memberId, requestBody = { imageDeleted: false }) => {
+    // 필수 전달 값 설정
+    if (!requestBody.imageDeleted) {
+        requestBody.imageDeleted = false;
+    }
+
+    return request({ method: METHOD.PATCH, url: `/members/${memberId}`, body: requestBody });
+};
