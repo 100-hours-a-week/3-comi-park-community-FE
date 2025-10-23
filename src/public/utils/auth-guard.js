b11e75b53c0d3daadd2e1e98766f838c511c35e0
kmi0817 <kmi0817@naver.com>
2025-10-23 16:45:48 +0900

    [feat] 인증 정보 검증하는 유틸리티 함수 작성

---
+++ src/public/utils/auth-guard.js
@@ -0,0 +1,19 @@
+import { requestAuth } from '../api/auth.js';
+
+/**
+ * 인증 정보가 있는지, 있다면 유효한지 여부를 서버에 확인한 후
+ * 유효한 로그인 상태가 아니라면 로그인 페이지로 이동합니다
+ *
+ * 활용) 로그인 정보가 필요한 페이지의 DOMContentLoaded 이벤트 핸들러에서 아래 두 코드 삽입
+ *  const { success, loginMemberId } = await getAuth();
+ *  if (!success) return;
+ */
+export const getAuth = async () => {
+    const res = await requestAuth();
+
+    if (!res.success) {
+        location.replace('/login');
+    }
+
+    return { success: res.success, loginMemberId: res.data?.auth?.id };
+};
