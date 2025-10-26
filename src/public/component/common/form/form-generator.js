6462fd937ca26ae13f94c6ce103b5c8ad2d774a8
kmi0817 <kmi0817@naver.com>
2025-10-26 23:25:55 +0900

    [feat] form 내 태그 html을 동적으로 반환하는 모듈 생성

---
+++ src/public/component/common/form/form-generator.js
@@ -0,0 +1,159 @@
+const defaultFieldAttributes = {
+    /* members */
+    email: {
+        name: '이메일',
+        type: 'email',
+        fieldname: 'email',
+        keyname: 'email',
+        isRequired: true,
+        isUnique: true,
+    },
+    password: {
+        name: '비밀번호',
+        type: 'password',
+        fieldname: 'password',
+        keyname: 'password',
+        isRequired: true,
+        isUnique: false,
+    },
+    confirmedPassword: {
+        name: '비밀번호 확인',
+        type: 'password',
+        fieldname: 'confirmedPassword',
+        keyname: 'password',
+        isRequired: true,
+        isUnique: false,
+    },
+    nickname: {
+        name: '닉네임',
+        type: 'text',
+        fieldname: 'nickname',
+        keyname: 'nickname',
+        isRequired: true,
+        isUnique: true,
+    },
+    /* posts */
+    title: {
+        name: '제목',
+        type: 'text',
+        fieldname: 'title',
+        keyname: 'title',
+        isRequired: true,
+        isUnique: false,
+    },
+    content: {
+        name: '내용',
+        fieldname: 'content',
+        keyname: 'content',
+        isRequired: true,
+        rows: 25,
+    },
+    /* 댓글 */
+    comment: {
+        name: '댓글',
+        fieldname: 'content',
+        keyname: 'comment',
+        isRequired: true,
+        rows: 8,
+    },
+    /* 이미지 */
+    'member-image': {
+        keyname: 'member-image',
+        name: '프로필 이미지',
+        imageHtml: (image) =>
+            `<img class="form-member-image round-image" src="${image?.url ?? '/assets/default-profile-image.png'}" width="250" height="250" alt="member image" />`,
+        isBtnOverlay: true,
+    },
+    'post-image': {
+        keyname: 'post-image',
+        name: '이미지',
+        imageHtml: (image) =>
+            `<input type="text" class="form-post-image" value="${image?.filename ?? '등록된 이미지가 없습니다'}" disabled />`,
+        isBtnOverlay: false,
+    },
+};
+
+const generateInputDivHtml = (attributes, value = '') => {
+    // 수정일 땐 isValidated = true
+    return `
+        <div>
+            <label for="form-${attributes.keyname}-input" class="form-label">${attributes.name}</label>
+            <input
+                name="${attributes.name}"
+                type="${attributes?.type ?? 'text'}"
+                class="form-input ${attributes?.isRequired ? 'required' : ''}"
+                id="form-${attributes.keyname}-input"
+                placeholder="${attributes.name} 값을 입력해주세요"
+                data-validated="${!!value ? true : false}"
+                data-ischanged="false"
+                data-fieldname="${attributes.fieldname}"
+                data-keyname="${attributes.keyname}"
+                value="${value}"
+            />
+            <div class="form-helper-text form-helper-${attributes.keyname}"></div>
+        </div>`;
+};
+
+const generateTextareaDivHtml = (attributes, value = '') => {
+    return `
+        <div>
+            <label for="form-${attributes.keyname}-input" class="form-label">${attributes.name}</label>
+            <textarea
+                name="${attributes.name}"
+                class="form-input ${attributes?.isRequired ? 'required' : ''}"
+                id="form-${attributes.keyname}-input"
+                placeholder="${attributes.name} 값을 입력해주세요"
+                data-validated="${!!value ? true : false}"
+                data-ischanged="false"
+                data-fieldname="${attributes.fieldname}"
+                data-keyname="${attributes.keyname}"
+                data-unique="${attributes?.isUnique ?? false}"
+                rows="${attributes?.rows ?? 8}"
+            >${value}</textarea>
+            <div class="form-helper-text form-helper-${attributes.keyname}"></div>
+        </div>`;
+};
+
+const generateImageDivHtml = (attributes, image = {}) => {
+    return `
+        <div>
+            <label for="form-${attributes.keyname}-input" class="form-label">${attributes.name}</label>
+            <div class="form-${attributes.keyname}-container">
+                ${attributes.imageHtml(image)}
+                <div class="${attributes.isBtnOverlay}">
+                    <button type="button" class="btn form-image-update-btn">변경</button>
+                    <button type="button" class="btn form-image-delete-btn">삭제</button>
+                </div>
+            </div>
+            <input
+                name="${attributes.name}"
+                type="file"
+                id="form-${attributes.keyname}-input"
+                data-ischanged="false"
+                data-fieldname="image"
+                data-keyname="${attributes.keyname}"
+                style="display: none"
+                value="${JSON.stringify({ id: image?.id, objectKey: image?.objectKey })}"
+            />
+            <div class="form-helper-text form-helper-${attributes.keyname}"></div>
+        </div>`;
+};
+
+const formFieldDivHtmlGenerator = {
+    email: generateInputDivHtml,
+    password: generateInputDivHtml,
+    confirmedPassword: generateInputDivHtml,
+    nickname: generateInputDivHtml,
+    title: generateInputDivHtml,
+
+    content: generateTextareaDivHtml,
+    comment: generateTextareaDivHtml,
+
+    'member-image': generateImageDivHtml,
+    'post-image': generateImageDivHtml,
+};
+
+export const generateFormFieldDivHtml = (keyname, value = '') => {
+    const generator = formFieldDivHtmlGenerator[keyname];
+    return !generator ? '' : generator(defaultFieldAttributes[keyname], value);
+};
