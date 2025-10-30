0036a0f94a25efcc621fe7595340cc8163387523
kmi0817 <kmi0817@naver.com>
2025-10-30 17:28:54 +0900

    [refactor] 새로 작성한 form 모듈 사용하도록 수정

--- src/public/post-write/post-write.js
+++ src/public/post-write/post-write.js
@@ -1,6 +1,7 @@
 import { paintHeader } from '../component/common/header/header.js';
-import { paintPostForm } from '../component/post/post.js';
+import { paintForm } from '../component/common/form/form-painter.js';
 import { getAuth } from '../utils/auth-guard.js';
+import { requestWritePost } from '../api/posts.js';
 
 document.addEventListener('DOMContentLoaded', async () => {
     const { success, loginMemberId } = await getAuth();
@@ -8,5 +9,24 @@ document.addEventListener('DOMContentLoaded', async () => {
     if (!success) return;
 
     paintHeader(success, loginMemberId);
-    paintPostForm();
+
+    const sectionElement = document.querySelector('section');
+    sectionElement.insertAdjacentHTML('beforeend', `<div class="title">게시글 작성</div>`);
+
+    paintForm({
+        formParent: sectionElement,
+        fields: ['title', 'content', 'post-image'],
+        submitBtnValue: '등록',
+        afterSubmit: async (requestBody) => {
+            const res = await requestWritePost(requestBody);
+
+            if (!res.success) {
+                // TODO: 오른쪽 상단에 토스 메시지 띄우기
+                alert(res.data);
+                return;
+            }
+
+            location.replace(`/read/${res.data.post.id}`);
+        },
+    });
 });
