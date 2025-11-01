a123468b5e632c79a8758ae7b8beeb280c3bae26
kmi0817 <kmi0817@naver.com>
2025-11-01 19:39:52 +0900

    [fix] 게시글 무한 스크롤링 시 불러올 데이터 없으면 더 이상 서버에 요청 보내지 않도록 수정

--- src/public/index/index.js
+++ src/public/index/index.js
@@ -4,15 +4,6 @@ import { paintPostContainer } from '../component/post/posts.js';
 import { requestPosts } from '../api/posts.js';
 import { getAuth } from '../utils/auth-guard.js';
 
-const postsContainerClickHandler = (target) => {
-    const postContainer = target.closest('.post-container');
-
-    if (!postContainer) return;
-
-    const postId = postContainer.dataset.postid;
-    location.href = `/read/${postId}`;
-};
-
 document.addEventListener('DOMContentLoaded', async () => {
     const { success, loginMemberId } = await getAuth();
 
@@ -20,26 +11,28 @@ document.addEventListener('DOMContentLoaded', async () => {
 
     paintHeader(success, loginMemberId);
 
+    const bodyElement = document.querySelector('body');
+    const mainElement = bodyElement.querySelector('main');
+    const postsContainerElement = mainElement.querySelector('.posts-container');
+
     const res = await requestPosts();
 
     if (!res.success) {
-        document.querySelector('.posts-container').textContent = res.data;
+        postsContainerElement.textContent = res.data;
         return;
     }
 
     paintPostContainer(res.data.posts);
 
-    document.querySelector('.posts-container').addEventListener('click', ({ target }) => {
-        postsContainerClickHandler(target);
-    });
-
-    const bodyElement = document.querySelector('body');
-    const mainElement = bodyElement.querySelector('main');
     paintFooter(bodyElement, mainElement);
 
     let isNewPostFetching = false;
+    const postElements = Array.from(postsContainerElement.querySelectorAll('[data-postid]'));
+    let lastPostId = postElements.length > 0 ? postElements.at(-1).dataset.postid : -1;
 
     window.addEventListener('scroll', async () => {
+        if (lastPostId === -1) return;
+
         const hitsBottom = window.innerHeight + window.scrollY >= document.documentElement.scrollHeight - 1;
 
         if (!hitsBottom || isNewPostFetching) {
@@ -48,14 +41,20 @@ document.addEventListener('DOMContentLoaded', async () => {
 
         isNewPostFetching = true;
 
-        const lastPostId = Array.from(document.querySelectorAll('[data-postid]')).at(-1).dataset.postid;
         const res = await requestPosts({ lastPostId });
 
         if (!res.success) {
-            document.querySelector('.posts-container').textContent = res.data;
+            // TODO: 오른쪽 상단에 토스 메시지 띄우기
+            alert(res.data);
             return;
         }
 
+        if (res.data.posts.length === 0 || res.data.posts.at(-1).id === lastPostId) {
+            lastPostId = -1;
+            return;
+        }
+
+        lastPostId = res.data.posts.at(-1).id;
         paintPostContainer(res.data.posts);
         isNewPostFetching = false;
     });
