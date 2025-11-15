0a4580d5bacdb554fe6819db06d84b0f90c38165
kmi0817 <kmi0817@naver.com>
2025-11-15 18:21:31 +0900

    [feat] 라우팅 제거

--- src/app.js
+++ src/app.js
@@ -6,38 +6,6 @@ const port = process.env.PORT;
 
 app.use(express.static(path.join(import.meta.dirname, 'public')));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(import.meta.dirname, 'public', 'index.html'));
-});
-
-app.get('/login', (req, res) => {
-    res.sendFile(path.join(import.meta.dirname, 'public', 'login', 'index.html'));
-});
-
-app.get('/register', (req, res) => {
-    res.sendFile(path.join(import.meta.dirname, 'public', 'register', 'index.html'));
-});
-
-app.get('/write', (req, res) => {
-    res.sendFile(path.join(import.meta.dirname, 'public', 'write', 'index.html'));
-});
-
-app.get('/read', (req, res) => {
-    res.sendFile(path.join(import.meta.dirname, 'public', 'read', 'index.html'));
-});
-
-app.get('/update', (req, res) => {
-    res.sendFile(path.join(import.meta.dirname, 'public', 'update', 'index.html'));
-});
-
-app.get('/account', (req, res) => {
-    res.sendFile(path.join(import.meta.dirname, 'public', 'account', 'index.html'));
-});
-
-app.get('/account-password', (req, res) => {
-    res.sendFile(path.join(import.meta.dirname, 'public', 'account', 'index.html'));
-});
-
 app.listen(port, () => {
     console.log(`Express runs on port ${port}`);
 });
