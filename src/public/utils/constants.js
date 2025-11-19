6ff858b8e8f133e125dc623e6acf28e762060f23
kmi0817 <kmi0817@naver.com>
2025-11-19 20:24:53 +0900

    [feat] 이미지 업로드 요청 경로가 로컬호스트일 때와 아닐 때를 구분함

--- src/public/utils/constants.js
+++ src/public/utils/constants.js
@@ -2,6 +2,10 @@ export const API_SERVER_URI = window.location.hostname.includes('localhost')
     ? 'http://localhost:8080/api'
     : 'https://api.kbt-comi.store/api';
 
+export const IMAGE_UPLOAD_URI = window.location.hostname.includes('localhost')
+    ? 'http://localhost:8080/api'
+    : 'https://upload.kbt-comi.store';
+
 export const DEFAULT_MEMBER_IMAGE = '/assets/default-profile-image.png';
 
 export const FOOTER_HEIGHT = '52px';
